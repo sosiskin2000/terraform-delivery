51515f8974c16c9477704f8f90611ce3c90a25f0
opc <anton.guts@oracle.com>
2021-10-13 23:08:56 +0300

    fix full repo

--- README.md
+++
@@ -1,1 +0,0 @@
-# terform-delivery
--- modules/vcn/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    oci = {
-      source  = "hashicorp/oci"
-      version = ">=4.0.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
